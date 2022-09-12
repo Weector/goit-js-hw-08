9a2c8838e5e7b7a40518b8d94b74ea8dc337831b
Victor Grossu <weekgrossu@gmail.com>
2022-09-12 21:24:38 +0300

    HW 1 & 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryImageMarkup();
+
+galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryImageMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <li> <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>
+`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  const currentTime = data.seconds.toString();
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', throttle(onPlay, 1000));
